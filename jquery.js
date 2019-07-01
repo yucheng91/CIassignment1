5a811aca32b55f07476c8bf6a3b7397a783c0430
yucheng91 <ubuntu@ip-172-31-81-4.ec2.internal>
2019-07-01 08:56:57 +0000

    finalised style.css

--- jquery.js
+++ jquery.js
@@ -1,15 +1,15 @@
 /* global $ */
 
 $("#thecarousel").hide();
-$("#upcomingshow").hide();
 $("#themembers").hide();
 $("#themusic").hide();
 $("#thevideo").hide();
+$("#schedule").hide();
 
 $(function(){
-    $("#thecarousel").fadeIn(2000);
-    $("#upcomingshow").fadeIn(2500);
+    $("#thecarousel").fadeIn(2500);
     $("#themembers").fadeIn(3000);
     $("#themusic").fadeIn(3500);
     $("#thevideo").fadeIn(4000);
+    $("#schedule").fadeIn(4500);
 })
